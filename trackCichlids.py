8298a3be2503026b063d387f2d6ff02e8d3890a0
Patrick McGrath <ptmcgrat@gmail.com>
2021-01-05 16:10:07 -0500

    Test

--- trackCichlids.py
+++ trackCichlids.py
@@ -1,5 +1,5 @@
 import argparse
-from Modules.Tracking.CichlidTracker import CichlidTracker as CT
+from Modules.CichlidTracker import CichlidTracker as CT
 
 parser = argparse.ArgumentParser(help='This command starts a script on a Raspberry Pis to collect depth and RGB data. Allows control through a Google Spreadsheet.')
 
