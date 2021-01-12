68bbddac30712367dde5ba777ffb4e941a48e4fb
Patrick McGrath <ptmcgrat@gmail.com>
2021-01-12 06:16:57 -0500

    Test

--- Modules/processVideo.py
+++ Modules/processVideo.py
@@ -13,7 +13,7 @@ if '.h264' not in args.VideoFile:
 	raise Exception(args.VideoFile + ' not an h264 file')
 
 # Convert h264 to mp4
-ffmpeg_output = subprocess.run(['ffmpeg', '-r', str(args.Framerate), '-i', args.VideoFile, '-threads', '1', '-c:v', 'copy', '-r', str(args.Framerate), args.VideoFile.replace('.h264', '.mp4')], capture_output = True, decoding = 'utf-8')
+ffmpeg_output = subprocess.run(['ffmpeg', '-r', str(args.Framerate), '-i', args.VideoFile, '-threads', '1', '-c:v', 'copy', '-r', str(args.Framerate), args.VideoFile.replace('.h264', '.mp4')])
 assert os.path.isfile(args.VideoFile.replace('.h264', '.mp4'))
 assert os.path.getsize(args.VideoFile.replace('.h264','.mp4')) > os.path.getsize(args.VideoFile)
 
