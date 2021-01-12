3e9641c5b68e81348e337b81fab03cfed5106bd0
Patrick McGrath <ptmcgrat@gmail.com>
2021-01-12 06:30:30 -0500

    Test

--- Modules/processVideo.py
+++ Modules/processVideo.py
@@ -13,12 +13,12 @@ if '.h264' not in args.VideoFile:
 	raise Exception(args.VideoFile + ' not an h264 file')
 
 # Convert h264 to mp4
-ffmpeg_output = subprocess.run(['ffmpeg', '-r', str(args.Framerate), '-i', args.VideoFile, '-threads', '1', '-c:v', 'copy', '-r', str(args.Framerate), args.VideoFile.replace('.h264', '.mp4')])
+#ffmpeg_output = subprocess.run(['ffmpeg', '-r', str(args.Framerate), '-i', args.VideoFile, '-threads', '1', '-c:v', 'copy', '-r', str(args.Framerate), args.VideoFile.replace('.h264', '.mp4')])
 assert os.path.isfile(args.VideoFile.replace('.h264', '.mp4'))
 assert os.path.getsize(args.VideoFile.replace('.h264','.mp4')) > os.path.getsize(args.VideoFile)
 
 # Sync with cloud (will return error if something goes wrong)
-FM.uploadData(args.VideoFile.replace('.h264', '.mp4'))
+fileManager.uploadData(args.VideoFile.replace('.h264', '.mp4'))
 
 # Delete videos
 #subprocess.run(['mv', args.VideoFile, '../Backups/'])
