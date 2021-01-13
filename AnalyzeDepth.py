8b971547f31f222c022d8e28c39ce7fc44878c55
Patrick McGrath <ptmcgrat@gmail.com>
2021-01-13 11:27:43 -0500

    Test

--- AnalyzeDepth.py
+++ AnalyzeDepth.py
@@ -1,10 +1,8 @@
-import argparse, os, pdb, sys
+import argparse, pdb
 from Modules.DataPreparers.ProjectPreparer import ProjectPreparer as PP
 
 parser = argparse.ArgumentParser(usage = 'This script will perform inital analysis of depth data to create a smoothed, interpolated, 3D npy array')
-
-parser.add_argument('ProjectIDs', type = str, help = 'Which projectID you want to identify')
-
+parser.add_argument('ProjectID', type = str, help = 'Which projectID you want to identify')
 args = parser.parse_args()
 
 pp_obj = PP(args.ProjectID)
