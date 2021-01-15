3dce27c7e81c9ddc49d89be238968d1860f816ec
Patrick McGrath <ptmcgrat@gmail.com>
2021-01-14 19:22:44 -0500

    Test

--- DownloadData.py
+++ DownloadData.py
@@ -2,7 +2,7 @@ import argparse, sys
 from Modules.DataPreparers.ProjectPreparer import ProjectPreparer as PP
 
 parser = argparse.ArgumentParser()
-parser.add_argument('DataType', type = str, choices=['Prep','Depth','Cluster','ClusterClassification','FishDetection','ManualAnnotation','Figures','All'], help = 'What type of analysis to perform')
+parser.add_argument('DataType', type = str, choices=['Prep','Depth','Cluster','ClusterClassification','FishDetection','ManualAnnotation','ManualLabelVideos','Figures','All'], help = 'What type of analysis to perform')
 parser.add_argument('ProjectID', type = str, help = 'Manually identify the projects you want to analyze. If All is specified, all non-prepped projects will be analyzed')
 parser.add_argument('-v', '--VideoIndex', type = int, help = 'Specify which video should be downloaded if Cluster analysis is to be performed')
 
