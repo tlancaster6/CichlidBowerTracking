33e1679baf424587772796c8f9acac2e6704660a
Patrick McGrath <ptmcgrat@gmail.com>
2021-02-16 14:10:23 -0500

    First commit

--- Modules/DataPreparers/ThreeDModelPreparer.py
+++ Modules/DataPreparers/ThreeDModelPreparer.py
@@ -41,7 +41,7 @@ class ThreeDModelPreparer():
 		command.extend(['--projectMeans'])
 		
 		#command = "source activate CichlidActionClassification; " + ' ' .join(command)
-		command = "source " + os.getenv('HOME') + "/anaconda3/etc/profile.d/conda.sh; conda activate CichlidActionClassification; conda info"
+		command = "source " + os.getenv('HOME') + "/anaconda3/etc/profile.d/conda.sh; conda activate CichlidActionClassification; " + ' '.join(command)
 		subprocess.run('bash -c \"' + command + '\"', shell = True)
 		os.chdir('..')
 
