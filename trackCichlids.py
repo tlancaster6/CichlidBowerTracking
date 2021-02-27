45bd2d0331bd448fed97932244edddd99e5875c0
ptmcgrat <patrick.mcgrath@biology.gatech.edu>
2021-02-27 08:28:05 -0500

    Delete trackCichlids.py

--- trackCichlids.py
+++
@@ -1,8 +0,0 @@
-import argparse
-from Modules.CichlidTracker import CichlidTracker as CT
-
-parser = argparse.ArgumentParser(description='This command starts a script on a Raspberry Pis to collect depth and RGB data. Allows control through a Google Spreadsheet.')
-
-args = parser.parse_args()
-	
-CT()
