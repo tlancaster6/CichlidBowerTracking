bd5424984d7465e68339545657294de077d85eb6
Patrick McGrath <ptmcgrat@gmail.com>
2021-02-27 08:41:47 -0500

    Test

--- fixData.py
+++ fixData.py
@@ -10,54 +10,55 @@ parser.add_argument('--SummaryFile', type = str, help = 'Name of summary file to
 
 args = parser.parse_args()
 
-if args.ProjectIDs is not None:
-	for projectID in args.ProjectIDs:
-		subprocess.run(['python3', 'Modules/UnitScripts/DownloadData.py','Prep', '--ProjectID', projectID])
-		subprocess.run(['python3', 'Modules/UnitScripts/PrepData.py', projectID])
-		subprocess.run(['python3', 'Modules/UnitScripts/UploadData.py','Prep', projectID])
 
+
+if args.ProjectIDs is not None:
+	projects = args.ProjectIDs
 else:
 	dt = pd.read_csv(args.SummaryFile, index_col = 0)
-	for projectID in dt[dt.StartingFiles == False].projectID:
-		fm_obj = FM(projectID = projectID)
-		print(projectID)
-		# Download and read log file
-		fm_obj.downloadData(fm_obj.localLogfile)
-		lp = LP(fm_obj.localLogfile)
+	projects = dt[dt.StartingFiles == False].projectID
 
-		fm_obj.createDirectory(fm_obj.localPrepDir)
 
-		data_dirs = subprocess.run(['rclone', 'lsf', 'cichlidVideo:McGrath/Apps/CichlidPiData/' + projectID], capture_output = True, encoding = 'utf-8').stdout.split('\n')
+for projectID in projects:
+	fm_obj = FM(projectID = projectID)
+	print(projectID)
+	# Download and read log file
+	fm_obj.downloadData(fm_obj.localLogfile)
+	lp = LP(fm_obj.localLogfile)
 
-		if 'Frames.tar' in data_dirs:
-			fm_obj.downloadData(fm_obj.localFrameDir, tarred = True)
-			fix_Frames = False
-		elif 'Frames/' in data_dirs:
-			fm_obj.downloadData(fm_obj.localFrameDir, tarred = False)
-			fix_Frames = True
-		else:
-			pdb.set_trace()
+	fm_obj.createDirectory(fm_obj.localPrepDir)
 
-		for movie in lp.movies:
-			if movie.startTime.hour > 8 and movie.startTime.hour < 18:
-				depthObj = [x for x in lp.frames if x.time > movie.startTime][0]
-				break
-		
-		shutil.copy2(fm_obj.localProjectDir + depthObj.npy_file, fm_obj.localPrepDir + 'FirstDepth.npy')
-		shutil.copy2(fm_obj.localProjectDir + depthObj.pic_file, fm_obj.localPrepDir + 'DepthRGB.jpg')
+	data_dirs = subprocess.run(['rclone', 'lsf', 'cichlidVideo:McGrath/Apps/CichlidPiData/' + projectID], capture_output = True, encoding = 'utf-8').stdout.split('\n')
 
-		fm_obj.downloadData(fm_obj.localProjectDir + movie.pic_file)
-		shutil.copy2(fm_obj.localProjectDir + movie.pic_file, fm_obj.localPrepDir + 'PiCameraRGB.jpg')
+	if 'Frames.tar' in data_dirs:
+		fm_obj.downloadData(fm_obj.localFrameDir, tarred = True)
+		fix_Frames = False
+	elif 'Frames/' in data_dirs:
+		fm_obj.downloadData(fm_obj.localFrameDir, tarred = False)
+		fix_Frames = True
+	else:
+		pdb.set_trace()
 
-		i = -1
-		while not os.path.exists(fm_obj.localProjectDir + lp.frames[i].npy_file):
-			i -= 1
-
-		shutil.copy2(fm_obj.localProjectDir + lp.frames[i].npy_file, fm_obj.localPrepDir + 'FirstDepth.npy')
+	for movie in lp.movies:
+		if movie.startTime.hour > 8 and movie.startTime.hour < 18:
+			depthObj = [x for x in lp.frames if x.time > movie.startTime][0]
+			break
 	
-		fm_obj.uploadData(fm_obj.localPrepDir)
+	shutil.copy2(fm_obj.localProjectDir + depthObj.npy_file, fm_obj.localPrepDir + 'FirstDepth.npy')
+	shutil.copy2(fm_obj.localProjectDir + depthObj.pic_file, fm_obj.localPrepDir + 'DepthRGB.jpg')
+
+	fm_obj.downloadData(fm_obj.localProjectDir + movie.pic_file)
+	shutil.copy2(fm_obj.localProjectDir + movie.pic_file, fm_obj.localPrepDir + 'PiCameraRGB.jpg')
+
+	i = -1
+	while not os.path.exists(fm_obj.localProjectDir + lp.frames[i].npy_file):
+		i -= 1
+
+	shutil.copy2(fm_obj.localProjectDir + lp.frames[i].npy_file, fm_obj.localPrepDir + 'FirstDepth.npy')
+
+	fm_obj.uploadData(fm_obj.localPrepDir)
 
 
-		if fix_Frames:
-			fm_obj.uploadData(fm_obj.localFrameDir, tarred = True)
+	if fix_Frames:
+		fm_obj.uploadData(fm_obj.localFrameDir, tarred = True)
 
