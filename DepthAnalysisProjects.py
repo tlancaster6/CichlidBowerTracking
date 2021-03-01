18a3d0d20d8244cb44dcb791491c6814cfb47f49
Patrick McGrath <patrick.mcgrath@biology.gatech.edu>
2021-03-01 09:00:08 -0500

    New

--- DepthAnalysisProjects.py
+++ DepthAnalysisProjects.py
@@ -14,7 +14,7 @@ if args.ProjectIDs is not None:
 		subprocess.run(['python3', 'Modules/UnitScripts/UploadData.py','Depth', projectID])
 		subprocess.run(['python3', 'Modules/UnitScripts/DeleteData.py', projectID])
 
-else:
+elif args.SummaryFile is not None:
 	dt = pd.read_csv(args.SummaryFile, index_col = 0)
 	for projectID in dt[dt.DepthFiles == False].projectID:
 		subprocess.run(['python3', 'Modules/UnitScripts/DownloadData.py','Depth', '--ProjectID', projectID])
@@ -24,3 +24,7 @@ else:
 
 		dt.loc[dt.projectID == projectID,'DepthFiles'] = True
 		dt.to_csv(args.SummaryFile)
+		pdb.set_trace()
+
+else:
+	print('Must use one of two options')
