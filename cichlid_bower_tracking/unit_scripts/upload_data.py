7400621a7e327acf9f4cc0f8b1826dbd524ac0cd
Patrick McGrath <ptmcgrat@gmail.com>
2021-05-07 07:29:03 -0400

    Test

--- cichlid_bower_tracking/unit_scripts/upload_data.py
+++ cichlid_bower_tracking/unit_scripts/upload_data.py
@@ -6,7 +6,7 @@ import argparse,sys
 from cichlid_bower_tracking.data_preparers.project_preparer import ProjectPreparer as PP
 
 parser = argparse.ArgumentParser()
-parser.add_argument('DataType', type = str, choices=['Prep','Depth','Video', 'Cluster','ClusterClassification','FishDetection','ManualAnnotation','Summary','All'], help = 'What type of analysis to perform')
+parser.add_argument('DataType', type = str, choices=['Prep','Depth','Cluster','ClusterClassification','FishDetection','ManualAnnotation','Summary','All'], help = 'What type of analysis to perform')
 parser.add_argument('ProjectID', type = str, help = 'Manually identify the projects you want to analyze. If All is specified, all non-prepped projects will be analyzed')
 parser.add_argument('-v', '--VideoIndex', type = int, help = 'Specify which video should be downloaded if Cluster analysis is to be performed')
 parser.add_argument('-d', '--Delete', action = 'store_true', help = 'Delete data once it is uploaded')
@@ -16,11 +16,8 @@ args = parser.parse_args()
 print('Uploading data for ' + args.ProjectID, file = sys.stderr)
 
 pp_obj = PP(args.ProjectID)
-if args.DataType in ['Cluster','Video']:
-	if args.VideoIndex is None:
-		print('Must specify a video index if downloading data for Cluster Analysis')
-	pp_obj.uploadData(args.DataType, videoIndex = args.VideoIndex)
-else:
-	pp_obj.uploadData(args.DataType)
+
+pp_obj.uploadData(args.DataType, args.VideoIndex)
+
 if args.Delete:
 	pp_obj.localDelete()
