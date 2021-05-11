f6eb8aae2908a9001088ca324a4b84d4a6d8c9c9
Patrick McGrath <patrick.mcgrath@biology.gatech.edu>
2021-05-11 06:43:56 -0400

    Getting started commits

--- cichlid_bower_tracking/runAnalysis.py
+++ cichlid_bower_tracking/runAnalysis.py
@@ -34,7 +34,7 @@ else:
 
 # To run analysis efficiently, we download and upload data in the background while the main script runs
 uploadProcesses = [] # Keep track of all of the processes still uploading so we don't quit before they finish
-print('Downloading: ' + projectIDs[0] + ' ' + str(datetime.datetime.now()))
+#print('Downloading: ' + projectIDs[0] + ' ' + str(datetime.datetime.now()))
 subprocess.run(['python3', '-m', 'cichlid_bower_tracking.unit_scripts.download_data',args.AnalysisType, '--ProjectID', projectIDs[0], '--ModelID', args.ModelID])
 for i, projectID in enumerate(projectIDs):
 	print('Running: ' + projectID + ' ' + str(datetime.datetime.now()))
@@ -71,7 +71,7 @@ for i, projectID in enumerate(projectIDs):
 	print('Uploading: ' + projectID + ' ' + str(datetime.datetime.now()))
 
 	#uploadProcesses.append(subprocess.Popen(['python3', '-m', 'cichlid_bower_tracking.unit_scripts.upload_data', args.AnalysisType, '--Delete', '--ProjectID', projectID]))
-	uploadProcesses.append(subprocess.Popen(['python3', '-m', 'cichlid_bower_tracking.unit_scripts.upload_data', args.AnalysisType, projectID]))
+	#uploadProcesses.append(subprocess.Popen(['python3', '-m', 'cichlid_bower_tracking.unit_scripts.upload_data', args.AnalysisType, projectID]))
 
 for i,p in enumerate(uploadProcesses):
 	print('Finishing uploading process ' + str(i) + ': ' + str(datetime.datetime.now()))
