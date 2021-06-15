43b903895eb4534053f04849cdc8f53d8f44603c
tlancaster6 <tuckerlancaster@gmail.com>
2021-06-15 11:34:38 -0400

    debugging failure to download euth data automatically

--- cichlid_bower_tracking/unit_scripts/download_data.py
+++ cichlid_bower_tracking/unit_scripts/download_data.py
@@ -6,12 +6,13 @@ parser.add_argument('DataType', type = str, choices=['Prep','Depth','Cluster','C
 parser.add_argument('--ProjectID', type = str, help = 'Manually identify the projects you want to analyze. If All is specified, all non-prepped projects will be analyzed')
 parser.add_argument('--VideoIndex', type = int, help = 'Specify which video should be downloaded if Cluster analysis is to be performed')
 parser.add_argument('--ModelID', type = str, help = 'Specify a ModelID to download')
+parser.add_argument('--SummaryFile', type = str, help = 'Name of csv file that specifies projects to analyze', default=None)
 
 args = parser.parse_args()
 
 #print('Downloading data for ' + args.ProjectID, file = sys.stderr)
 
 
-pp_obj = PP(args.ProjectID, args.ModelID)
+pp_obj = PP(args.ProjectID, args.ModelID, summaryFile=args.SummaryFile)
 pp_obj.downloadData(args.DataType, videoIndex = args.VideoIndex)
 
